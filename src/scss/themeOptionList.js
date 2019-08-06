abf7dbe344c67376c6f8070360002688098c899c
刘志伟 <lzw@spacetech.com.cn>
2019-08-06 10:45:10 +0800

    预设

---
+++ src/scss/themeOptionList.js
@@ -0,0 +1,14 @@
+export default {
+  morning: {
+    '--theme-primary': 'linear-gradient(#dbf5ff, #84ccf2)'
+  },
+  afternoon: {
+    '--theme-primary': 'linear-gradient(#ffcc5e, #ff9543)'
+  },
+  evening: {
+    '--theme-primary': 'linear-gradient(#dbf5ff, #84ccf2)'
+  },
+  night: {
+    '--theme-primary': 'linear-gradient(#dbf5ff, #84ccf2)'
+  }
+}
