c613fcb267a70b33ff448c37429785a0acaedefc
刘志伟 <lzw@spacetech.com.cn>
2020-06-19 18:09:46 +0800

    更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
+  env: {node: true},
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ],
+  parserOptions: {ecmaVersion: 2020},
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
